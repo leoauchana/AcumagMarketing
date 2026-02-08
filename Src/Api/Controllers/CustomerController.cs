25c32f2803c59df4a8463284a512e0d751b96c7a
Leoauchana <matiasleonelcja@gmail.com>
2026-02-07 23:56:31 -0300

    feat: Implementación de controlador de clientes

--- Src/Api/Controllers/CustomerController.cs
+++ Src/Api/Controllers/CustomerController.cs
@@ -1,3 +1,4 @@
+using Application.DTOs;
 using Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,5 +13,36 @@ public class CustomerController : ControllerBase
     {
         _customers = customers;
     }
+
+    [HttpGet("/getAll")]
+    public async Task<IActionResult> GetAll()
+    {
+        var customers = await _customers.GetAllCustomers();
+        return Ok(customers);
+    }
+    [HttpGet("/getById/{id}")]
+    public async Task<IActionResult> GetById(string id)
+    {
+        var customer = await _customers.GetCustomerById(id);
+        return Ok(customer);
+    }
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CustomerDto.Request customerDto)
+    {
+        var customer = await _customers.Create(customerDto);
+        return Ok(customer);
+    }
     
+    [HttpDelete("/{id}")]
+    public async Task<IActionResult> Delete(string id)
+    { 
+        await _customers.Delete(id);
+        return Ok("The customer was successfully deleted.");
+    }
+    [HttpPatch]
+    public async Task<IActionResult> Create([FromBody] CustomerDto.RequestUpdate customerDto)
+    {
+        var customer = await _customers.Update(customerDto);
+        return Ok(customer);
+    }
 }
